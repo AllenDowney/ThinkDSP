418001f60d5507db24655c61c88be630113e1788
Allen Downey <downey@allendowney.com>
2016-01-21 09:48:19 -0500

    Updating Chapter 5 code

--- code/autocorr.py
+++ code/autocorr.py
@@ -10,13 +10,12 @@ from __future__ import print_function, division
 import thinkdsp
 import thinkplot
 import thinkstats2
-import math
-import numpy
+import numpy as np
 
-PI2 = 2 * math.pi
+PI2 = 2 * np.pi
 
 
-def make_wave(offset):
+def make_sine(offset):
     """Makes a 440 Hz sine wave with the given phase offset.
 
     offset: phase offset in radians
@@ -38,62 +37,61 @@ def corrcoef(xs, ys):
 
     returns: float
     """
-    return numpy.corrcoef(xs, ys, ddof=0)[0, 1]
+    return np.corrcoef(xs, ys, ddof=0)[0, 1]
 
 
 def plot_sines():
     """Makes figures showing correlation of sine waves with offsets.
     """
-    wave1 = make_wave(0)
-    wave2 = make_wave(offset=1)
+    wave1 = make_sine(0)
+    wave2 = make_sine(offset=1)
 
     thinkplot.preplot(2)
     wave1.segment(duration=0.01).plot(label='wave1')
     wave2.segment(duration=0.01).plot(label='wave2')
 
-    corr_matrix = numpy.corrcoef(wave1.ys, wave2.ys, ddof=0)
+    corr_matrix = np.corrcoef(wave1.ys, wave2.ys, ddof=0)
     print(corr_matrix)
 
     thinkplot.save(root='autocorr1',
-                   xlabel='time (s)',
-                   ylabel='amplitude',
+                   xlabel='Time (s)',
                    ylim=[-1.05, 1.05])
 
-
-    offsets = numpy.linspace(0, PI2, 101)
+    offsets = np.linspace(0, PI2, 101)
 
     corrs = []
     for offset in offsets:
-        wave2 = make_wave(offset)
+        wave2 = make_sine(offset)
         corr = corrcoef(wave1.ys, wave2.ys)
         corrs.append(corr)
     
     thinkplot.plot(offsets, corrs)
     thinkplot.save(root='autocorr2',
-                   xlabel='offset (radians)',
-                   ylabel='correlation',
+                   xlabel='Offset (radians)',
+                   ylabel='Correlation',
                    xlim=[0, PI2],
                    ylim=[-1.05, 1.05])
 
 
-def plot_shifted(wave, shift=0.002, start=0.2):
+def plot_shifted(wave, offset=0.002, start=0.2):
     """Plots two segments of a wave with different start times.
 
     wave: Wave
-    shift: difference in start time (seconds)
+    offset: difference in start time (seconds)
     start: start time in seconds
     """
     thinkplot.preplot(num=2)
     segment1 = wave.segment(start=start, duration=0.01)
     segment1.plot(linewidth=2, alpha=0.8)
 
-    segment2 = wave.segment(start=start-shift, duration=0.01)
+    segment2 = wave.segment(start=start-offset, duration=0.01)
+    segment2.shift(offset)
     segment2.plot(linewidth=2, alpha=0.4)
 
     corr = segment1.corr(segment2)
     text = r'$\rho =$ %.2g' % corr
-    thinkplot.text(0.0005, -0.8, text)
-    thinkplot.config(xlabel='time (s)', ylim=[-1, 1])
+    thinkplot.text(start+0.0005, -0.8, text)
+    thinkplot.config(xlabel='Time (s)', ylim=[-1.05, 1.05])
 
 
 def serial_corr(wave, lag=1):
@@ -114,9 +112,9 @@ def serial_corr(wave, lag=1):
 def plot_serial_corr():
     """Makes a plot showing serial correlation for pink noise.
     """
-    numpy.random.seed(19)
+    np.random.seed(19)
 
-    betas = numpy.linspace(0, 2, 21)
+    betas = np.linspace(0, 2, 21)
     corrs = []
 
     for beta in betas:
@@ -126,8 +124,8 @@ def plot_serial_corr():
         corrs.append(corr)
 
     thinkplot.plot(betas, corrs)
-    thinkplot.config(xlabel=r'pink noise parameter, $\beta$',
-                     ylabel='serial correlation', 
+    thinkplot.config(xlabel=r'Pink noise parameter, $\beta$',
+                     ylabel='Serial correlation', 
                      ylim=[0, 1.05])
     thinkplot.save(root='autocorr3')
 
@@ -157,16 +155,16 @@ def plot_pink_autocorr(beta, label):
 def plot_autocorr():
     """Plots autocorrelation for pink noise with different parameters
     """
-    numpy.random.seed(19)
+    np.random.seed(19)
     thinkplot.preplot(3)
 
     for beta in [1.7, 1.0, 0.3]:
         label = r'$\beta$ = %.1f' % beta
         plot_pink_autocorr(beta, label)
 
-    thinkplot.config(xlabel='lag', 
-                     ylabel='correlation', 
-                     xlim=[-1, 1000], 
+    thinkplot.config(xlabel='Lag', 
+                     ylabel='Correlation', 
+                     xlim=[-5, 1000], 
                      ylim=[-0.05, 1.05])
     thinkplot.save(root='autocorr4')
 
@@ -181,37 +179,37 @@ def plot_singing_chirp():
     segment = wave.segment(start=0.2, duration=duration)
 
     # plot two copies of the wave with a shift
-    plot_shifted(wave, start=0.2, shift=0.0023)
+    plot_shifted(wave, start=0.2, offset=0.0023)
     thinkplot.save(root='autocorr7')
 
     # plot the autocorrelation function
     lags, corrs = autocorr(segment)
     thinkplot.plot(lags, corrs)
-    thinkplot.config(xlabel='lag (index)', 
-                     ylabel='correlation', 
-                     ylim=[-1, 1],
+    thinkplot.config(xlabel='Lag (index)', 
+                     ylabel='Correlation', 
+                     ylim=[-1.05, 1.05],
                      xlim=[0, 225])
     thinkplot.save(root='autocorr8')
 
     # plot the spectrogram
     gram = wave.make_spectrogram(seg_length=1024)
-    gram.plot(high=100)
+    gram.plot(high=4200)
 
-    thinkplot.config(xlabel='time (s)', 
-                     ylabel='frequency (Hz)',
+    thinkplot.config(xlabel='Time (s)', 
+                     ylabel='Frequency (Hz)',
                      xlim=[0, 1.4],
                      ylim=[0, 4200])
     thinkplot.save(root='autocorr5')
     
     # plot the spectrum of one segment
     spectrum = segment.make_spectrum()
-    spectrum.plot(high=16)
-    thinkplot.config(xlabel='frequency (Hz)', ylabel='amplitude')
+    spectrum.plot(high=1000)
+    thinkplot.config(xlabel='Frequency (Hz)', ylabel='Amplitude')
     thinkplot.save(root='autocorr6')
 
 
 def plot_correlate():
-    """Plots the autocorrelation function computed by numpy.
+    """Plots the autocorrelation function computed by np.
     """
     wave = thinkdsp.read_wave('28042__bcjordan__voicedownbew.wav')
     wave.normalize()
@@ -219,27 +217,22 @@ def plot_correlate():
 
     lags, corrs = autocorr(segment)
 
-    corrs2 = numpy.correlate(segment.ys, segment.ys, mode='same')
-    thinkplot.plot(corrs2)
-    thinkplot.config(xlabel='lag', 
-                     ylabel='correlation', 
-                     xlim=[0, len(corrs2)])
+    N = len(segment)
+    corrs2 = np.correlate(segment.ys, segment.ys, mode='same')
+    lags = np.arange(-N//2, N//2)
+    thinkplot.plot(lags, corrs2)
+    thinkplot.config(xlabel='Lag', 
+                     ylabel='Correlation', 
+                     xlim=[-N//2, N//2])
     thinkplot.save(root='autocorr9')
-
-    N = len(corrs2)
-    half = corrs2[N//2:]
-
-    lengths = range(N, N//2, -1)
-    half /= lengths
-    half /= half[0]
 
 
 def main():
-    #plot_sines()
-    #plot_serial_corr()
+    plot_sines()
+    plot_serial_corr()
     plot_autocorr()
-    #plot_singing_chirp()
-    #plot_correlate()
+    plot_singing_chirp()
+    plot_correlate()
 
 
 if __name__ == '__main__':
