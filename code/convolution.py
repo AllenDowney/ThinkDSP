d600709e0d617efaa5a0a19970547b08528c3cd5
Allen Downey <downey@allendowney.com>
2016-01-27 10:58:34 -0500

    Updating chap08 code

--- code/convolution.py
+++ code/convolution.py
@@ -7,27 +7,28 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 
 from __future__ import print_function, division
 
-import math
-import numpy
-import scipy.signal
-import pandas
-
 import thinkdsp
 import thinkplot
 
+import numpy as np
+import pandas as pd
 
-PI2 = math.pi * 2
+import scipy.signal
+
+PI2 = np.pi * 2
 
 
 def plot_bitcoin():
+    """Plot BitCoin prices and a smoothed time series.
+    """
     nrows = 1625
     df = pandas.read_csv('coindesk-bpi-USD-close.csv', 
                          nrows=nrows, parse_dates=[0])
     ys = df.Close.values
 
-    window = numpy.ones(30)
+    window = np.ones(30)
     window /= sum(window)
-    smoothed = numpy.convolve(ys, window, mode='valid')
+    smoothed = np.convolve(ys, window, mode='valid')
 
     N = len(window)
     smoothed = thinkdsp.shift_right(smoothed, N//2)
@@ -37,22 +38,37 @@ def plot_bitcoin():
     thinkplot.config(xlabel='time (days)', 
                      ylabel='price',
                      xlim=[0, nrows],
-#                     ylim=[-60, 60],
                      loc='lower right')
     thinkplot.save(root='convolution1')
 
 
-def zero_pad(array, n):
-    """Makes a new array with the same elements and the given length.
+GRAY = "0.7"
 
-    array: numpy array
-    n: length of result
-
-    returns: new NumPy array
+def plot_facebook():
+    """Plot Facebook prices and a smoothed time series.
     """
-    res = numpy.zeros(n)
-    res[:len(array)] = array
-    return res
+    names = ['date', 'open', 'high', 'low', 'close', 'volume']
+    df = pd.read_csv('fb.csv', header=0, names=names, parse_dates=[0])
+    close = df.close.values[::-1]
+    dates = df.date.values[::-1]
+    days = (dates - dates[0]) / np.timedelta64(1,'D')
+
+    M = 30
+    window = np.ones(M)
+    window /= sum(window)
+    smoothed = np.convolve(close, window, mode='valid')
+    smoothed_days = days[M//2: len(smoothed) + M//2]
+    
+    thinkplot.plot(days, close, color=GRAY, label='daily close')
+    thinkplot.plot(smoothed_days, smoothed, label='30 day average')
+    
+    last = days[-1]
+    thinkplot.config(xlabel='Time (days)', 
+                     ylabel='Price ($)',
+                     xlim=[-7, last+7],
+                     legend=True,
+                     loc='lower right')
+    thinkplot.save(root='convolution1')
 
 
 def plot_boxcar():
@@ -63,53 +79,53 @@ def plot_boxcar():
     wave = signal.make_wave(duration=1, framerate=44100)
 
     # and a boxcar window
-    window = numpy.ones(11)
+    window = np.ones(11)
     window /= sum(window)
 
     # select a short segment of the wave
     segment = wave.segment(duration=0.01)
 
     # and pad with window out to the length of the array
-    padded = zero_pad(window, len(segment))
+    N = len(segment)
+    padded = thinkdsp.zero_pad(window, N)
 
     # compute the first element of the smoothed signal
     prod = padded * segment.ys
     print(sum(prod))
 
     # compute the rest of the smoothed signal
-    smoothed = numpy.zeros_like(segment.ys)
+    smoothed = np.zeros(N)
     rolled = padded
-    for i in range(len(segment.ys)):
+    for i in range(N):
         smoothed[i] = sum(rolled * segment.ys)
-        rolled = numpy.roll(rolled, 1)
+        rolled = np.roll(rolled, 1)
 
     # plot the results
-    segment.plot(color='0.7')
+    segment.plot(color=GRAY)
     smooth = thinkdsp.Wave(smoothed, framerate=wave.framerate)
     smooth.plot()
-    thinkplot.config(ylim=[-1.05, 1.05], legend=False)
+    thinkplot.config(xlabel='Time(s)', ylim=[-1.05, 1.05])
     thinkplot.save(root='convolution2')
 
-    # compute the same thing using numpy.convolve
-    segment.plot(color='0.7')
-    ys = numpy.convolve(segment.ys, window, mode='valid')
+    # compute the same thing using np.convolve
+    segment.plot(color=GRAY)
+    ys = np.convolve(segment.ys, window, mode='valid')
     smooth2 = thinkdsp.Wave(ys, framerate=wave.framerate)
     smooth2.plot()
-    thinkplot.config(ylim=[-1.05, 1.05], legend=False)
+    thinkplot.config(xlabel='Time(s)', ylim=[-1.05, 1.05])
     thinkplot.save(root='convolution3')
 
     # plot the spectrum before and after smoothing
     spectrum = wave.make_spectrum()
-    spectrum.plot(color='0.7')
+    spectrum.plot(color=GRAY)
 
-    ys = numpy.convolve(wave.ys, window, mode='same')
+    ys = np.convolve(wave.ys, window, mode='same')
     smooth = thinkdsp.Wave(ys, framerate=wave.framerate)
     spectrum2 = smooth.make_spectrum()
     spectrum2.plot()
-    thinkplot.config(xlabel='frequency (Hz)',
-                     ylabel='amplitude',
-                     xlim=[0, 22050], 
-                     legend=False)
+    thinkplot.config(xlabel='Frequency (Hz)',
+                     ylabel='Amplitude',
+                     xlim=[0, 22050])
     thinkplot.save(root='convolution4')
 
     # plot the ratio of the original and smoothed spectrum
@@ -119,24 +135,22 @@ def plot_boxcar():
     ratio[amps<560] = 0
     thinkplot.plot(ratio)
 
-    thinkplot.config(xlabel='frequency (Hz)',
-                     ylabel='amplitude ratio',
-                     xlim=[0, 22050], 
-                     legend=False)
+    thinkplot.config(xlabel='Frequency (Hz)',
+                     ylabel='Amplitude ratio',
+                     xlim=[0, 22050])
     thinkplot.save(root='convolution5')
 
 
     # plot the same ratio along with the FFT of the window
-    padded = zero_pad(window, len(wave))
-    dft_window = numpy.fft.rfft(padded)
+    padded = thinkdsp.zero_pad(window, len(wave))
+    dft_window = np.fft.rfft(padded)
 
-    thinkplot.plot(abs(dft_window), color='0.7', label='boxcar filter')
+    thinkplot.plot(abs(dft_window), color=GRAY, label='boxcar filter')
     thinkplot.plot(ratio, label='amplitude ratio')
 
-    thinkplot.config(xlabel='frequency (Hz)',
-                     ylabel='amplitude ratio',
-                     xlim=[0, 22050], 
-                     legend=False)
+    thinkplot.config(xlabel='Frequency (Hz)',
+                     ylabel='Amplitude ratio',
+                     xlim=[0, 22050])
     thinkplot.save(root='convolution6')
 
     
@@ -149,7 +163,7 @@ def plot_gaussian():
     spectrum = wave.make_spectrum()
 
     # and a boxcar window
-    boxcar = numpy.ones(11)
+    boxcar = np.ones(11)
     boxcar /= sum(boxcar)
 
     # and a gaussian window
@@ -159,11 +173,10 @@ def plot_gaussian():
     thinkplot.preplot(2)
     thinkplot.plot(boxcar, label='boxcar')
     thinkplot.plot(gaussian, label='Gaussian')
-    thinkplot.config(xlabel='index',
-                     ylabel='amplitude')
+    thinkplot.config(xlabel='Index', legend=True)
     thinkplot.save(root='convolution7')
 
-    ys = numpy.convolve(wave.ys, gaussian, mode='same')
+    ys = np.convolve(wave.ys, gaussian, mode='same')
     smooth = thinkdsp.Wave(ys, framerate=wave.framerate)
     spectrum2 = smooth.make_spectrum()
 
@@ -174,86 +187,81 @@ def plot_gaussian():
     ratio[amps<560] = 0
 
     # plot the same ratio along with the FFT of the window
-    padded = zero_pad(gaussian, len(wave))
-    dft_gaussian = numpy.fft.rfft(padded)
+    padded = thinkdsp.zero_pad(gaussian, len(wave))
+    dft_gaussian = np.fft.rfft(padded)
 
-    thinkplot.plot(abs(dft_gaussian), color='0.7', label='Gaussian filter')
+    thinkplot.plot(abs(dft_gaussian), color=GRAY, label='Gaussian filter')
     thinkplot.plot(ratio, label='amplitude ratio')
 
-    thinkplot.config(xlabel='frequency (Hz)',
-                     ylabel='amplitude ratio',
-                     xlim=[0, 22050], 
-                     legend=False)
+    thinkplot.config(xlabel='Frequency (Hz)',
+                     ylabel='Amplitude ratio',
+                     xlim=[0, 22050])
     thinkplot.save(root='convolution8')
 
 
 def fft_convolve(signal, window):
-    fft_signal = numpy.fft.fft(signal)
-    fft_window = numpy.fft.fft(window)
-    return numpy.fft.ifft(fft_signal * fft_window)
+    fft_signal = np.fft.fft(signal)
+    fft_window = np.fft.fft(window)
+    return np.fft.ifft(fft_signal * fft_window)
 
 
 def fft_autocorr(signal):
     N = len(signal)
-    window = signal[::-1]
-    signal = zero_pad(signal, 2*N)
-    window = zero_pad(window, 2*N)
+    signal = thinkdsp.zero_pad(signal, 2*N)
+    window = np.flipud(signal)
 
     corrs = fft_convolve(signal, window)
-    corrs = corrs[N//2: 3*N//2]
+    corrs = np.roll(corrs, N//2+1)[:N]
     return corrs
 
 
 def plot_fft_convolve():
     """Makes a plot showing that FFT-based convolution works.
     """
-    df = pandas.read_csv('coindesk-bpi-USD-close.csv', 
-                         nrows=1625, 
-                         parse_dates=[0])
-    ys = df.Close.values
+    names = ['date', 'open', 'high', 'low', 'close', 'volume']
+    df = pd.read_csv('fb.csv', header=0, names=names, parse_dates=[0])
+    close = df.close.values[::-1]
 
-    # compute a 30-day average using numpy.convolve
+    # compute a 30-day average using np.convolve
     window = scipy.signal.gaussian(M=30, std=6)
     window /= window.sum()
-    smoothed = numpy.convolve(ys, window, mode='valid')
+    smoothed = np.convolve(close, window, mode='valid')
 
     # compute the same thing using fft_convolve
-    padded = zero_pad(window, len(ys))
-    smoothed2 = fft_convolve(ys, padded)
+    N = len(close)
+    padded = thinkdsp.zero_pad(window, N)
+
     M = len(window)
-    smoothed2 = smoothed2[M-1:]
+    smoothed4 = fft_convolve(close, padded)[M-1:]
 
     # check for the biggest difference
-    diff = smoothed - smoothed2
+    diff = smoothed - smoothed4
     print(max(abs(diff)))
 
-    # compute autocorrelation using numpy.correlate
-    N = len(ys)
-    corrs = numpy.correlate(ys, ys, mode='same')
-    corrs = corrs[N//2:]
-
-    corrs2 = fft_autocorr(ys)
-    corrs2 = corrs2[N//2:]
+    # compute autocorrelation using np.correlate
+    corrs = np.correlate(close, close, mode='same')
+    corrs2 = fft_autocorr(close)
 
     # check for the biggest difference
     diff = corrs - corrs2
     print(max(abs(diff)))
 
     # plot the results
-    thinkplot.preplot(1)
-    thinkplot.plot(corrs, color='0.7', linewidth=7, label='numpy.convolve')
-    thinkplot.plot(corrs2.real, linewidth=2, label='fft_convolve')
-    thinkplot.config(xlabel='lags', 
-                     ylabel='correlation', 
-                     xlim=[0, N//2])
+    lags = np.arange(N) - N//2
+    thinkplot.plot(lags, corrs, color=GRAY, linewidth=7, label='np.convolve')
+    thinkplot.plot(lags, corrs2.real, linewidth=2, label='fft_convolve')
+    thinkplot.config(xlabel='Lag', 
+                     ylabel='Correlation', 
+                     xlim=[-N//2, N//2])
     thinkplot.save(root='convolution9')
 
 
 def main():
-    plot_gaussian()
+    plot_facebook()
     plot_boxcar()
-    plot_bitcoin()
+    plot_gaussian()
     plot_fft_convolve()
+    #plot_bitcoin()
 
 
 if __name__ == '__main__':
