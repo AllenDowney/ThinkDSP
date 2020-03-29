cf65c5c62840dcb0ca3cb39bb5164784df3cb4a4
Allen Downey <downey@allendowney.com>
2020-03-29 11:25:09 -0400

    Adding rfft tests

--- code/thinkdsp_test.py
+++ code/thinkdsp_test.py
@@ -12,7 +12,50 @@ import thinkdsp
 
 import numpy as np
 
+
 class Test(unittest.TestCase):
+    def assertArrayAlmostEqual(self, a, b):
+        total_error = np.sum(np.abs(a-b))
+        self.assertAlmostEqual(total_error, 0)
+
+    def testMakeSpectrum(self):
+        # rfft with n even
+        ys = np.arange(10)
+        wave = thinkdsp.Wave(ys, framerate=10)
+        spectrum = wave.make_spectrum()
+        self.assertAlmostEqual(spectrum.hs[0], 45)
+        wave2 = spectrum.make_wave()
+        #print(wave2.ys)
+        self.assertArrayAlmostEqual(wave.ys, wave2.ys)
+
+        # rfft with n odd
+        ys = np.arange(11)
+        wave = thinkdsp.Wave(ys, framerate=10)
+        spectrum = wave.make_spectrum()
+        self.assertAlmostEqual(spectrum.hs[0], 55)
+
+        # TODO: make rfft invertible when n is odd
+        #wave2 = spectrum.make_wave()
+        #print(wave2.ys)
+        #self.assertArrayAlmostEqual(wave.ys, wave2.ys)
+
+        # fft with n even
+        ys = np.arange(10)
+        wave = thinkdsp.Wave(ys, framerate=10)
+        spectrum = wave.make_spectrum(full=True)
+        self.assertAlmostEqual(spectrum.hs[0], 45)
+        wave2 = spectrum.make_wave()
+        #print(wave2.ys)
+        self.assertArrayAlmostEqual(wave.ys, wave2.ys)
+
+        # fft with n odd
+        ys = np.arange(11)
+        wave = thinkdsp.Wave(ys, framerate=10)
+        spectrum = wave.make_spectrum(full=True)
+        self.assertAlmostEqual(spectrum.hs[0], 55)
+        wave2 = spectrum.make_wave()
+        #print(wave2.ys)
+        self.assertArrayAlmostEqual(wave.ys, wave2.ys)
 
     def testComplexSinusoid(self):
         signal = thinkdsp.ComplexSinusoid(440, 0.7, 1.1)
@@ -34,16 +77,16 @@ class Test(unittest.TestCase):
         signal = thinkdsp.CosSignal(freq=2)
         wave = signal.make_wave(duration=1, framerate=8)
         dct = wave.make_dct()
-        
+
         self.assertAlmostEqual(dct.fs[0], 0.25)
 
     def testImpulses(self):
-        imp_sig = thinkdsp.Impulses([0.01, 0.4, 0.8, 1.2], 
+        imp_sig = thinkdsp.Impulses([0.01, 0.4, 0.8, 1.2],
                                     amps=[1, 0.5, 0.25, 0.1])
-        impulses = imp_sig.make_wave(start=0, duration=1.3, 
+        impulses = imp_sig.make_wave(start=0, duration=1.3,
                                      framerate=11025)
 
-        self.assertAlmostEqual(len(impulses), 14333)
+        self.assertEqual(len(impulses), 14332)
 
 
 if __name__ == "__main__":
