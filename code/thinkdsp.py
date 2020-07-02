77af13ba16adfd40cd6a1f832b5e47c268b13051
Allen Downey <downey@allendowney.com>
2020-07-02 16:20:05 -0400

    Update thinkdsp.py

--- code/thinkdsp.py
+++ code/thinkdsp.py
@@ -2,7 +2,7 @@
 by Allen B. Downey, available from greenteapress.com
 
 Copyright 2013 Allen B. Downey
-License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+License: MIT License (https://opensource.org/licenses/MIT)
 """
 
 import copy
