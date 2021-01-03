204e1cfd7943e2e2a1788c38f80cc92a31004615
marksweiss <marksimonweiss@gmail.com>
2021-01-02 22:51:36 -0500

    Finish ch_02 ex_4

--- exercises/ch_02/ex_4_spectrum_0th_index_value.py
+++ exercises/ch_02/ex_4_spectrum_0th_index_value.py
@@ -1,3 +1,11 @@
+# Make a triangle signal with frequency 440 and make a Wave with duration 0.01 seconds.
+#  Plot the waveform.
+# Make a Spectrum object and print spectrum.hs[0]. What is the amplitude and phase of
+#  this component?
+# Set spectrum.hs[0] = 100. What effect does this operation have on the waveform?
+#  Hint: Spectrum provides a method called make_wave that computes the Wave that
+#  corresponds to the Spectrum.
+
 from code.thinkdsp import TriangleSignal, decorate
 from exercises.lib.lib import play_wave
 
@@ -11,15 +19,16 @@ def run():
     start = 0.
     duration_secs = 0.01
     segment = triangle_sig.make_wave(start=0, duration=duration_secs, framerate=FRAMERATE)
-
-    # import pdb; pdb.set_trace()
-
     segment.plot()
     decorate(xlabel='Time (s)')
 
-    # print('Playing square signal without aliasing')
-    # play_wave(wave)
+    spectrum = segment.make_spectrum()
+    print(f'spectrum.hs[0] = {spectrum.hs[0]}')
 
+    spectrum.hs[0] = 100.
+    wave = spectrum.make_wave()
+    wave.plot()
+    decorate(xlabel='Time (s)')
 
 
 if __name__ == '__main__':
