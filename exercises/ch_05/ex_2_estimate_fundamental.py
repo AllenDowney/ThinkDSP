6e12427f61fbf65f0f0d195b07af5cea9c97c73d
marksweiss <marksimonweiss@gmail.com>
2021-01-14 03:45:38 -0500

    Code cleanup

--- exercises/ch_05/ex_2_estimate_fundamental.py
+++ exercises/ch_05/ex_2_estimate_fundamental.py
@@ -14,45 +14,54 @@ FRAMERATE = 22050
 
 
 def estimate_fundemental(wave):
-    # Compute sequency of correlation coefficients for each pair of segments in the wave
-    # Pairs are the wave up to time interval t and from t to the end, for each interval in lags
-    correlations = np.correlate(wave.ys, wave.ys, mode='same')
-    # Normalize the correlation
-    # np.correlate uses the unstandardized definition of correlation; as the lag gets bigger,
-    #  the number of points in the overlap between the two signals gets smaller,
-    #  so the magnitude of the correlations decreases.
-    # We can correct that by dividing through by the lengths
-    n = len(correlations)
-    lengths = list(range(n, n // 2, -1))
-    pos_half = correlations[n // 2:]
-    # normalize so correlation with lag == 0 is 1
-    pos_half /= pos_half[0]
+    def compute_and_normalize_correlation(wave):
+        # Compute sequency of correlation coefficients for each pair of segments in the wave
+        # Pairs are the wave up to time interval t and from t to the end, for each interval in lags
+        correlation = np.correlate(wave.ys, wave.ys, mode='same')
+        # Normalize the correlation
+        # np.correlate uses the unstandardized definition of correlation; as the lag gets bigger,
+        #  the number of points in the overlap between the two signals gets smaller,
+        #  so the magnitude of the correlations decreases.
+        # We can correct that by dividing through by the lengths
+        n = len(correlation)
+        lengths = list(range(n, n // 2, -1))
+        pos_half = correlation[n // 2:]
+        # normalize so correlation with lag == 0 is 1
+        pos_half /= pos_half[0]
+        return pos_half
 
-    # Find first and second peak in the correlations, this is the period of the wave
-    # Use first and second because first is arbitrarily offset from the start of the wave
-    #  but if the wave is periodic and a high enough frequency to have at least one complete
-    #  cycle this algorithm finds that first comlete cycle (the first two peaks). The distance
-    #  between them is the distance of the period, i.e. the inverse of the frequency.
-    first_peak = None
-    second_peak = None
-    prev_val = pos_half[0]
-    cur_val = 0
-    for i in range(len(pos_half) - 1):
-        cur_val = pos_half[i]
-        next_val = pos_half[i + 1]
-        if first_peak is None and prev_val < cur_val < next_val:
-            first_peak = cur_val
-        elif second_peak is None and prev_val < cur_val < next_val:
-            second_peak = cur_val
-            break
-        prev_val = cur_val
-    if first_peak is None or second_peak is None:
-        print("Failed to find peaks in correlations. Cannot estimate fundamental")
-        return None
+    def find_period_peaks(correlation):
+        # Find first and second peak in the correlations, this is the period of the wave
+        # Use first and second because first is arbitrarily offset from the start of the wave
+        #  but if the wave is periodic and a high enough frequency to have at least one complete
+        #  cycle this algorithm finds that first comlete cycle (the first two peaks). The distance
+        #  between them is the distance of the period, i.e. the inverse of the frequency.
+        first_peak = None
+        second_peak = None
+        prev_val = correlation[0]
+        cur_val = 0
+        for i in range(len(correlation) - 1):
+            cur_val = correlation[i]
+            next_val = correlation[i + 1]
+            if first_peak is None and prev_val < cur_val < next_val:
+                first_peak = cur_val
+            elif second_peak is None and prev_val < cur_val < next_val:
+                second_peak = cur_val
+                break
+            prev_val = cur_val
+        if first_peak is None or second_peak is None:
+            print("Failed to find peaks in correlations. Cannot estimate fundamental")
+            return None
+        return first_peak, second_peak
 
-    lag_diff = second_peak - first_peak
-    period = lag_diff / wave.framerate
-    frequency = 1 / period
+    def compute_frequency(first_peak, second_peak):
+        lag_diff = second_peak - first_peak
+        period = lag_diff / wave.framerate
+        return 1 / period
+
+    correlation = compute_and_normalize_correlation(wave)
+    first_peak, second_peak = find_period_peaks(correlation)
+    frequency = compute_frequency(first_peak, second_peak)
 
     return frequency
 
